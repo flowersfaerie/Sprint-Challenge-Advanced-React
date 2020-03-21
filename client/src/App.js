9a8a9c333efaf7aeb7592c0f6c9a1204aa5cbf16
flowersfaerie <flowersfaerie@gmail.com>
2020-03-21 11:41:02 -0700

    Created useLocalStorage. Set up class on App.js. Create cards component for the Players

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import Player from './components/Player';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+      playerName: [],
+      playerCountry: country,
+      playerSearched: searches
+    };
+ 
+  // componentDidMount() {
+  //   fetch("http://localhost:5000/api/players")
+  //     .then(res => res.json())
+  //     .then(players => {
+  //       console.log("sf: App.js: CDM: .then: res: ", players);
+  //       this.state({
+  //         playerName: players.name,
+  //         playerCountry: players.country,
+  //         playerSearched: players.searches
+  //       });
+  //     })
+  //     .catch(err => console.error(err));
+  // }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/api/players")
+      .then(res => {
+        console.log("sf: App.js: CDM: .then: res: ", res)
+        this.setState({ 
+          playerName: res.name,
+          playerCountry: res.country,
+          playerSearched: res.searches})
+      })
+      .catch(err => console.error(err));
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <header className="App-header">
+          {this.state.playerName.map(player => {
+            return <Player key={playerName} playerName={playerName} />
+          })}
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
